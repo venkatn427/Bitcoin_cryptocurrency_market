5cc8922b18295fe3d3509f631b4fdf7d00d10244
venkatn427 <47615476+venkatn427@users.noreply.github.com>
2019-02-18 10:57:43 -0500

    Update Bitcoin.py

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,32 +1,132 @@
-1. Bitcoin. Cryptocurrencies. So hot right now.
-Since the launch of Bitcoin in 2008, hundreds of similar projects based on the blockchain technology have emerged. 
-We call these cryptocurrencies (also coins or cryptos in the Internet slang). Some are extremely valuable nowadays, and others may have the potential to become extremely valuable in the future1. 
-In fact, the 6th of December of 2017 Bitcoin has a market capitalization above $200 billion.
-
-The astonishing increase of Bitcoin market capitalization in 2017.
-1- WARNING: The cryptocurrency market is exceptionally volatile and any money you put in might disappear into thin air. 
-Cryptocurrencies mentioned here might be scams similar to Ponzi Schemes or have many other issues (overvaluation, technical, etc.). 
-Please do not mistake this for investment advice.
-
-That said, let's get to business. As a first task, we will load the current data from the coinmarketcap API and display it in the output.
-
 # Importing pandas
 import pandas as pd
-​
+
 # Importing matplotlib and setting aesthetics for plotting later.
 import matplotlib.pyplot as plt
 %matplotlib inline
 %config InlineBackend.figure_format = 'svg' 
 plt.style.use('fivethirtyeight')
-​
+
 # Reading in current data from coinmarketcap.com
 current = pd.read_json("https://api.coinmarketcap.com/v1/ticker/")
-​
+
 # Printing out the first few lines
 current.head()
-24h_volume_usd	available_supply	id	last_updated	market_cap_usd	max_supply	name	percent_change_1h	percent_change_24h	percent_change_7d	price_btc	price_usd	rank	symbol	total_supply
-0	6.189495e+09	17538500	bitcoin	1550191046	63428107592	2.100000e+07	Bitcoin	-0.05	-0.49	6.33	1.000000	3616.506976	1	BTC	17538500
-1	3.266116e+09	104862328	ethereum	1550191038	12744088393	NaN	Ethereum	-0.19	-1.28	15.98	0.033669	121.531618	2	ETH	104862328
-2	4.151599e+08	41208093050	ripple	1550191023	12430819688	1.000000e+11	XRP	-0.28	-0.86	3.33	0.000084	0.301660	3	XRP	99991698361
-3	9.760551e+08	60471000	litecoin	1550191025	2524284455	8.400000e+07	Litecoin	-0.06	-1.04	25.49	0.011564	41.743720	4	LTC	60471000
-4	9.083497e+08	906245118	eos	1550191024	2499505958	NaN	EOS	0.03	-4.06	17.53	0.000764	2.758090	5	EOS	1006245120
+
+# Reading datasets/coinmarketcap_06122017.csv into pandas
+dec6 = pd.read_csv('datasets/coinmarketcap_06122017.csv')
+
+# Selecting the 'id' and the 'market_cap_usd' columns
+market_cap_raw = dec6[['id','market_cap_usd']]
+
+# Counting the number of values
+market_cap_raw.head()
+
+# Filtering out rows without a market capitalization
+cap = market_cap_raw.query('market_cap_usd > 0')
+
+#Declaring these now for later use in the plots
+TOP_CAP_TITLE = 'Top 10 market capitalization'
+TOP_CAP_YLABEL = '% of total cap'
+
+# Selecting the first 10 rows and setting the index
+cap10 = cap.head(10).set_index('id')
+
+# Calculating market_cap_perc
+cap10 = cap10.assign(market_cap_perc = lambda x: (x.market_cap_usd / cap.market_cap_usd.sum()) *100)
+
+# Plotting the barplot with the title defined above 
+ax = cap10.market_cap_perc.head(10).plot.bar(title = TOP_CAP_TITLE)
+
+# Annotating the y axis with the label defined above
+ax.set_ylabel(TOP_CAP_YLABEL)
+
+# Colors for the bar plot
+COLORS = ['orange', 'green', 'orange', 'cyan', 'cyan', 'blue', 'silver', 'orange', 'red', 'green']
+
+# Plotting market_cap_usd as before but adding the colors and scaling the y-axis  
+ax = cap10.market_cap_perc.head(10).plot.bar(title = TOP_CAP_TITLE, color = COLORS)
+ax.set_yscale('log')
+# Annotating the y axis with 'USD'
+ax.set_ylabel('USD')
+
+# Final touch! Removing the xlabel as it is not very informative
+ax.set_xlabel('')
+
+
+# Selecting the id, percent_change_24h and percent_change_7d columns
+volatility = dec6[['id','percent_change_24h','percent_change_7d']]
+
+# Setting the index to 'id' and dropping all NaN rows
+volatility = volatility.set_index('id').dropna()
+
+# Sorting the DataFrame by percent_change_24h in ascending order
+volatility = volatility.sort_values(by = 'percent_change_24h', ascending = True)
+
+# Checking the first few rows
+volatility.head()
+
+#Defining a function with 2 parameters, the series to plot and the title
+def top10_subplot(volatility_series, title):
+    # Making the subplot and the figure for two side by side plots
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
+    
+    # Plotting with pandas the barchart for the top 10 losers
+    ax = volatility_series[:10].plot.bar(ax=axes[0], color='darkred')
+    
+    # Setting the figure's main title to the text passed as parameter
+    fig.suptitle(title)
+    
+    # Setting the ylabel to '% change'
+    ax.set_ylabel('% change')
+    
+    # Same as above, but for the top 10 winners
+    ax = volatility_series[-10:].plot.bar(ax=axes[1], color='darkblue')
+    
+    # Returning this for good practice, might use later
+    return fig, ax
+
+DTITLE = "24 hours top losers and winners"
+
+# Calling the function above with the 24 hours period series and title DTITLE  
+fig, ax = top10_subplot(volatility.percent_change_24h, DTITLE)
+
+# Sorting in ascending order
+volatility7d = volatility.sort_values(by = 'percent_change_7d', ascending = True)
+
+WTITLE = "Weekly top losers and winners"
+
+# Calling the top10_subplot function
+fig, ax = top10_subplot(volatility7d.percent_change_7d, WTITLE)
+
+# Selecting everything bigger than 10 billion 
+largecaps = cap.query('market_cap_usd > 1e+10')
+
+# Printing out largecaps
+largecaps.head()
+
+# Making a nice function for counting different marketcaps from the
+# "cap" DataFrame. Returns an int.
+# INSTRUCTORS NOTE: Since you made it to the end, consider it a gift :D
+def capcount(query_string):
+    return cap.query(query_string).count().id
+
+# Labels for the plot
+LABELS = ["biggish", "micro", "nano"]
+
+# Using capcount count the biggish cryptos
+biggish = capcount('market_cap_usd > 3e+8')
+
+# Same as above for micro ...
+micro = capcount('market_cap_usd > 5e+7' & 'market_cap_usd < 3e+8')
+
+# ... and for nano
+nano =  capcount('market_cap_usd < 5e+7')
+
+# Making a list with the 3 counts
+values = [biggish, micro, nano]
+
+# Plotting them with matplotlib 
+plt.bar(range(len(values)), values, tick_label = LABELS, color = ['darkred', 'darkgreen','darkblue'])
+plt.title('Classification of coin market')
+plt.ylabel('Number of coins')
